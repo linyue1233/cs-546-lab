aa66aac025feddbc101f690db1a6638d9fd6824a
linyue1233 <1191609469@qq.com>
2021-11-28 00:15:05 -0500

    lab11

--- lab9/public/palindromeCheck.js
+++ lab9/public/palindromeCheck.js
@@ -2,36 +2,36 @@
 
 let emptyError = document.getElementById("emptyError");
 let palidromeText = document.getElementById("palidromeText");
-// document.getElementById("formSubmit").addEventListener("submit", event => {
-//     event.preventDefault();
-//     let palidData = document.getElementById("palidromeText").value;
-//     if (palidData.trim() === "") {
-//         emptyError.hidden = false;
-//         document.getElementById("palidromeText").value = "";
-//         palidromeText.focus();
-//         return;
-//     } else {
-//         let lowerData = transferLower(palidData);
-//         if(lowerData === ""){
-//             emptyError.hidden = false;
-//             document.getElementById("palidromeText").value = "";
-//             palidromeText.focus();
-//             return;
-//         }
-//         // judge the string is palindrome
-//         let reversedData = lowerData.split("").reverse().join("");
-//         if (lowerData === reversedData) {
-//             emptyError.hidden = true;
-//             document.getElementById("attempts").innerHTML += "<li class = is-palindrome>" + palidData + "</li>";
-//         } else {
-//             emptyError.hidden = true;
-//             document.getElementById('attempts').innerHTML += "<li class = not-palindrome>" + palidData + "</li>";
-//         }
+document.getElementById("formSubmit").addEventListener("submit", event => {
+    event.preventDefault();
+    let palidData = document.getElementById("palidromeText").value;
+    if (palidData.trim() === "") {
+        emptyError.hidden = false;
+        document.getElementById("palidromeText").value = "";
+        palidromeText.focus();
+        return;
+    } else {
+        let lowerData = transferLower(palidData);
+        if(lowerData === ""){
+            emptyError.hidden = false;
+            document.getElementById("palidromeText").value = "";
+            palidromeText.focus();
+            return;
+        }
+        // judge the string is palindrome
+        let reversedData = lowerData.split("").reverse().join("");
+        if (lowerData === reversedData) {
+            emptyError.hidden = true;
+            document.getElementById("attempts").innerHTML += "<li class = is-palindrome>" + palidData + "</li>";
+        } else {
+            emptyError.hidden = true;
+            document.getElementById('attempts').innerHTML += "<li class = not-palindrome>" + palidData + "</li>";
+        }
 
-//         document.getElementById("palidromeText").value = "";
-//         return;
-//     }
-// })
+        document.getElementById("palidromeText").value = "";
+        return;
+    }
+})
 
 // function transferLower(textstr) {
 //     // build pattern regex
